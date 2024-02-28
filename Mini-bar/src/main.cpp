90df37f217bdd2bd638eb3ee803b48e6c8395c58
Sebas1705 <sebssgarcia502580@gmail.com>
2024-02-28 23:55:41 +0100

    Separete projects

--- Mini-bar/src/main.cpp
+++ Mini-bar/src/main.cpp
@@ -1,12 +1,49 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 
-BluetoothSerial b;
+//#define USE_PIN // Uncomment this to use PIN during pairing. The pin is specified on the line below
+const char *pin = "1234"; // Change this to more secure PIN.
+
+String device_name = "ESP32-BT-Slave";
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+#if !defined(CONFIG_BT_SPP_ENABLED)
+#error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
+#endif
+
+#define LED 2
+
+BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(115200);
-  b.begin("MyBluetooth");
+  SerialBT.begin(device_name); //Bluetooth device name
+  Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
+  //Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+  #ifdef USE_PIN
+    SerialBT.setPin(pin);
+    Serial.println("Using PIN");
+  #endif
+
+  pinMode(LED,OUTPUT);
 }
 
 void loop() {
+  if (Serial.available()) {
+    SerialBT.write(Serial.read());
+  }
+  if (SerialBT.available()) {
+    char in = SerialBT.read();
+    Serial.print("BT: ");
+    Serial.println(in);
+    if(in=='a'){
+      digitalWrite(LED,HIGH);
+    }else{
+      digitalWrite(LED,LOW);
+    }
+  }
+  delay(20);
 }
---
+++ mBapp/app/src/main/java/com/example/mbapp_androidapp/bluetoothInterface/BluetoothConnect.kt
@@ -0,0 +1,4 @@
+package com.example.mbapp_androidapp.bluetoothInterface
+
+class BluetoothConnect {
+}
