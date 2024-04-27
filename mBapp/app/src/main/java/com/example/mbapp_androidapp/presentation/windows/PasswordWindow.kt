95bcc6d26dfc0f41b1669a45516055e6e3631f6e
RubBen19 <98461533+RubBen19@users.noreply.github.com>
2024-04-27 13:21:33 +0200

    Update PasswordWindow.kt

--- mBapp/app/src/main/java/com/example/mbapp_androidapp/presentation/windows/PasswordWindow.kt
+++ mBapp/app/src/main/java/com/example/mbapp_androidapp/presentation/windows/PasswordWindow.kt
@@ -75,7 +75,7 @@ fun PasswordWindow(navController: NavHostController, flag: MutableState<Boolean>
                 //Botón para aceptar
                 IconButton(
                     onClick = {
-                        if (password == "Contraseña"){
+                        if (password == ""){
                             system.addLog("Acceso a modo empleado")
                             navController.navigate(AppScreens.EmployeeScreen.route)
                         }
